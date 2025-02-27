53b686287c3d967ae5194a781881b55cb5c5baaf
Cornelius Tanui <kiplimocornelius@gmail.com>
2025-02-27 18:28:47 +0300

    Add action page and implement genre selection callbacks in Dash app

--- pages/callbacks.py
+++ pages/callbacks.py
@@ -7,6 +7,7 @@
 # URL: https://filtera.onrender.com
 
 # Import required libraries
+import dash
 from dash import dcc, html, Input, Output, callback
 from app import app  # Import your Dash app instance
 from pages import render_page_content  # Ensure this function is correctly imported
@@ -20,3 +21,30 @@ def display_page(pathname):
     return render_page_content(pathname)
 
 
+# Define the callback to update the content based on the button clicked
+@dash.callback(
+    Output("content-area", "children"),
+    [Input("btn-action", "n_clicks"),
+     Input("btn-fantasy", "n_clicks"),
+     Input("btn-drama", "n_clicks"),
+     Input("btn-sci-fi", "n_clicks")]
+)
+
+def update_content(btn_action, btn_fantasy, btn_drama, btn_sci_fi):
+    ctx = dash.callback_context
+
+    if not ctx.triggered:
+        button_id = "btn-action"  # Default to Action content
+    else:
+        button_id = ctx.triggered[0]["prop_id"].split(".")[0]
+
+    if button_id == "btn-action":
+        return html.P("Content for Action genre.")
+    elif button_id == "btn-fantasy":
+        return html.P("Content for Fantasy genre.")
+    elif button_id == "btn-drama":
+        return html.P("Content for Drama genre.")
+    elif button_id == "btn-sci-fi":
+        return html.P("Content for Sci-Fi genre.")
+    else:
+        return html.P("Select a genre to see the content.")
